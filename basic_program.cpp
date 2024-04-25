07bc6a19b8d072736e60a20fd619e851f5971661
Tanvir Niaz <tanvirniaz@brainstation-23.com>
2024-04-25 14:07:16 +0600

    Conflict resolved arised while applying stashing main branch to feature-branch

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,12 +1,1 @@
-<<<<<<< Updated upstream
-#include <iostream>
-using namespace std;
- //hello world
-int main()
-{
-    cout << "Hello World";
-    return 0;
-}
-=======
 Basic program creation again
->>>>>>> Stashed changes
